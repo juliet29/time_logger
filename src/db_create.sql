a5f1a16668fd616cbfc872190879630d0c2373c6
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-12-06 15:21:25 -0800

    sql commands

--- src/db_create.sql
+++ src/db_create.sql
@@ -1,22 +1,22 @@
-CREATE TABLE entry (
-    entry_id INTEGER PRIMARY KEY,
-    entry_date TEXT,
-    entry_focus_area INTEGER FOREIGN KEY,
-    FOREIGN KEY(entry_focus_area) REFERENCES focus_area(focus_area_name),
-    entry_project_name INTEGER FOREIGN KEY,
-    FOREIGN KEY(entry_project_name) REFERENCES projects(project_name),
-    description TEXT
-);
 CREATE TABLE focus_area (
     focus_area_name TEXT PRIMARY KEY
 );
 CREATE TABLE projects (
     project_name TEXT PRIMARY KEY
 );
+CREATE TABLE entry (
+    entry_id INTEGER PRIMARY KEY,
+    entry_date TEXT,
+    entry_focus_area TEXT,
+    FOREIGN KEY(entry_focus_area) REFERENCES focus_area(focus_area_name),
+    entry_project_name TEXT,
+    FOREIGN KEY(entry_project_name) REFERENCES projects(project_name),
+    description TEXT
+);
 CREATE TABLE entered_activity (
     entered_activity_entry_id INTEGER PRIMARY KEY,
     FOREIGN KEY(entered_activity_entry_id) REFERENCES entry(entry_id),
-    entered_activity_activity_type_name INTEGER PRIMARY KEY,
+    entered_activity_activity_type_name TEXT PRIMARY KEY,
     FOREIGN KEY(entered_activity_activity_type_name) REFERENCES activity_type(activity_type_id)
 );
 CREATE TABLE activity_type(
