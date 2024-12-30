4018a113687f1154d086fb6b0bab087c643f66ef
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-12-30 07:59:35 +0000

    trouble plotting

--- src/db_commands/read.py
+++ src/db_commands/read.py
@@ -6,6 +6,11 @@ from variables import DB_PATH
 from db_commands.connect import connect_to_real_db
 from interfaces import ReturnedEntry
 from rich import print as rprint
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import pandas as pd
+import plotly.express as px
 
 MINUTES_TO_MS = 60*1000
 
@@ -28,18 +33,43 @@ def read_all_entries(connection: Connection):
         return df
 
     
-def create_list(df:pl.DataFrame):
+def aggregate_focus_project_date(df:pl.DataFrame):
     df = df.with_columns(
             minutes =MINUTES_TO_MS*pl.col("minutes").cast(pl.Duration(time_unit="ms"))
             )
     rprint(df)
     d = df.group_by("focus_area").agg(pl.col("minutes").sum())
     rprint(d)
-    d1 = df.group_by("focus_area", "project").agg(pl.col("minutes").sum())
-    rprint(d1)
+    d1 = df.group_by("focus_area", "project", "date").agg(pl.col("minutes").sum())
+    rprint(d1.dtypes)
+    rprint(d1.to_pandas())
+    rprint(d1.to_pandas().dtypes)
+
+    return d1
+
+def plot_focus_project_date(df:pl.DataFrame):
+    # sns.barplot(df.to_pandas(), x="date", y="minutes", hue="focus_area")
+    fig, ax = plt.subplots(nrows=1, figsize=(8, 6))
+    dfp = df.to_pandas()
+    # sns.barplot(df.to_pandas(), x="date", y="minutes", hue="focus_area", ax=ax)
+
+    dfp.plot(x="date", y="minutes", kind="bar", ax=ax, stacked=True)
+
+    ax.xaxis.set_ticks_position("bottom")
+    ax.xaxis.set_major_locator(ticker.AutoLocator())
+
+    # convert the ticks to string
+    ticks = ax.get_yticks()
+    ax.yaxis.set_major_locator(ticker.AutoLocator())
+    ax.set_yticklabels(pd.to_datetime(ticks, unit='ms').strftime('%H:%M'))
+    plt.show()
 
 
-    
+# def plot_focus_project_date_mpl(df:pl.DataFrame):
+#     dfp = df.to_pandas()
+#     fig = px.bar(df.to_pandas(), x='date', y='minutes', color="focus_area")
+#     fig.show(renderer="png")
+#     # fig.show()
    
 
 
@@ -47,5 +77,7 @@ def create_list(df:pl.DataFrame):
 if __name__ == "__main__":
     connection = connect_to_real_db(DB_PATH)
     df = read_all_entries(connection)
-    create_list(df)
+    df_agg = aggregate_focus_project_date(df)
+    plot_focus_project_date(df_agg)
+    # plot_focus_project_date_mpl(df_agg)
 
